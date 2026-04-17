be450cf7b3477f4bca43c8d063021a109a3b6b14
Quyến Black <havanan.8ph@gmail.com>
2026-04-17 11:52:17 +0700

    Số Lộc Phát là chỉ chứa số 6 hoặc 8
    Điều kiện xem chỉ có 6 hoạc 8 là số lộc phát

---
+++ BaiThucHanhChuong3LyThuyetBai.py
@@ -0,0 +1,16 @@
+print ('Danh Sach so nguyen n')
+n = int(input("Nhap so nguyen duong n : "))
+i = 0
+solocphat = True;
+while n > 0:
+    sodau = n % 10
+    n = n // 10
+    if (sodau != 6 and sodau != 8):
+        solocphat = False
+        break;
+    
+if solocphat == True:
+    print ('So loc phat  ')
+else :
+    print ('Khong phai so loc phat  ')
+
