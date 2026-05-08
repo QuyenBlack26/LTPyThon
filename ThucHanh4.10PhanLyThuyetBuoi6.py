d6dac19d977e9fc4023845860fee50e3ff4d6ce7
Quyến Black <havanan.8ph@gmail.com>
2026-05-08 10:14:37 +0700

    Set()
    xét một tập hợp số điện thoại và in ra các số điện thoại từ 0 đến 9  và in ra những số không có trong tập hợp mình nhập vào

---
+++ ThucHanh4.10PhanLyThuyetBuoi6.py
@@ -0,0 +1,8 @@
+#iii Viet chuong trinh cho nhap so dien thoai (s ) dunng tap hop. in ra cac so tu 0 den 9 khong xuat hien trong so dien thoai vua nhap
+# BaiThucHanh4.10PhanLyThuyetBuoi6.py
+s = input("Nhập số điện thoại: ")
+DanhDaySo = set('0123456789')
+DanhDaySoTrongSoDienThoai = set(s)
+SoKhongXuatHien = DanhDaySo - DanhDaySoTrongSoDienThoai
+print("Các số từ 0 đến 9 không xuất hiện trong số điện thoại vừa nhập là:", SoKhongXuatHien)
+
